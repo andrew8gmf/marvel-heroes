c588ed6bed2eb5a66268a1cd5a207eaf87f6807c
andrew8gmf <andrew8gmf@gmail.com>
2022-01-07 14:23:26 -0300

    feat: list of characters integrated with api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CharacterList } from '../models/character.list';
+import { CharacterService } from '../services/character.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,15 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public categories: Array<any> = [
+  public iconCategories: Array<any> = [
     { icon: '../../assets/icons/hero.svg', color: 'linear-gradient(180deg, #005BEA 0%, #00C6FB 100%)' },
     { icon: '../../assets/icons/villain.svg', color: 'linear-gradient(180deg, #ED1D24 0%, #ED1F69 100%)' },
     { icon: '../../assets/icons/antihero.svg', color: 'linear-gradient(180deg, #B224EF 0%, #7579FF 100%)' },
     { icon: '../../assets/icons/alien.svg', color: 'linear-gradient(180deg, #0BA360 0%, #3CBA92 100%)' },
     { icon: '../../assets/icons/human.svg', color: 'linear-gradient(180deg, #FF7EB3 0%, #FF758C 100%)' }
   ];
-  public slidesOptions: any = { slidesPerView: 5, freeMode: true }
+  public iconSlideOptions: any = { slidesPerView: 5, freeMode: true }
 
-  constructor() {}
+  public categories: Array<any> = [
+    { name: 'Heróis' },
+    { name: 'Vilões' },
+    { name: 'Anti-heróis' },
+    { name: 'Alienígenas' },
+    { name: 'Humanos' }
+  ];
+  public slideOptions: any = { slidesPerView: 2.5, freeMode: true }
+
+  characters: CharacterList[] = [];
+
+  constructor(private characterService: CharacterService) {
+    this.getCharacters();
+  }
+
+  getCharacters() {
+    this.characterService.getCharacters().subscribe(data => {
+      for (var key in data) {
+        this.characters.push(data[key])
+      }
+      console.log(this.characters)
+    });
+  }
 
 }
