7984b92256e92cc49a4267b0c0da131526806539
andrew8gmf <andrew8gmf@gmail.com>
2022-01-31 15:18:17 -0300

    fix: redirect characters page to first id page

--- src/app/characters/characters.page.ts
+++ src/app/characters/characters.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-characters',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.go();
+  }
 
   ngOnInit() {
+  }
+
+  go() {
+    this.router.navigate(['characters/1']);
   }
 
 }
