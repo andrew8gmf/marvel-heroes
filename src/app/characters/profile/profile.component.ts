34081c5ffe55df17effb5cafd076ca3baedb4fc4
andrew8gmf <andrew8gmf@gmail.com>
2022-02-11 16:39:27 -0300

    feat: integrate api with character id page

--- src/app/characters/profile/profile.component.ts
+++ src/app/characters/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CharacterService } from '../../services/character.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,37 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+
   id;
-  constructor(private route: ActivatedRoute) { }
+  character = [];
+  categories = [];
+
+  constructor(private route: ActivatedRoute, private characterService: CharacterService) {
+    this.getData();
+  }
 
   ngOnInit() {
     this.id = this.route.snapshot.paramMap.get('id');
+  }
+
+  getData() {
+    this.characterService.getData().subscribe(data => {
+      for (var key in data) {
+        this.categories.push(data[key])
+      }
+
+      for (var key in this.categories) {
+        this.character = this.categories[key].find(x => x.id == this.id)
+        if (this.character) {
+          console.log(this.character)
+          return this.character
+        }
+      }
+    });
+  }
+
+  getUrl(x,y) {
+    return x+y;
   }
 
 }
